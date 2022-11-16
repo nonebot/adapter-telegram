4cdd8116520e2e61e29fece7f4c1b05d0bcdfa79
jingyuexing <MayorTalor@163.com>
2022-11-16 22:23:19 +0800

    :memo: Add method document comment

--- nonebot/adapters/telegram/method.py
+++ nonebot/adapters/telegram/method.py
@@ -1,4 +1,12 @@
-from typing import Any, Dict, List, Union, Optional, Required
+from typing import (
+    Any,
+    Dict,
+    List,
+    Union,
+    Optional,
+    Required,
+    Literal
+)
 
 from .model import (
     File,
@@ -30,14 +38,32 @@ from .model import (
 
 
 def getMe() -> "User":
+    """
+    A simple method for testing your bot's authentication token. 
+    Requires no parameters. Returns basic information about the bot in form of a User object.
+    """
     pass
 
 
 def logOut():
+    """
+        Use this method to log out from the cloud Bot API server before launching the bot locally. 
+        You must log out the bot before running it locally, 
+        otherwise there is no guarantee that the bot will receive updates. After a successful call, 
+        you can immediately log in on a local server, 
+        but will not be able to log in back to the cloud Bot API server for 10 minutes. 
+        Returns True on success. Requires no parameters
+    """
     pass
 
 
 def close():
+    """
+        Use this method to close the bot instance before moving it from one local server to another. 
+        You need to delete the webhook before calling this method to ensure that the bot isn't launched again after server restart. 
+        The method will return error 429 in the first 10 minutes after the bot is launched. 
+        Returns True on success. Requires no parameters.
+    """
     pass
 
 
@@ -60,17 +86,17 @@ def sendMessage(
 ) -> "Message_":
     """[summary]
 
-    [description]
+    Use this method to send text messages. On success, the sent `Message` is returned.
 
-    Args:
-        chat_id (Union[str, int]): [description]
-        message_thread_id (Optional[int]): [description]
-        text (str): [description]
-        parse_mode (Optional[str]): [description]
-        entities (Optional[List[MessageEntity]]): [description]
-        disable_web_page_preview (Optional[bool]): [description]
-        disable_notification (Optional[bool]): [description]
-        protect_content (Optional[bool]): [description]
+    :Params:
+        chat_id (Union[str, int]): Unique identifier for the target chat or username of the target channel (in the format `@channelusername`)
+        message_thread_id (Optional[int]): Unique identifier for the target message thread (topic) of the forum; for forum supergroups only
+        text (str): Text of the message to be sent, 1-4096 characters after entities parsing
+        parse_mode (Optional[str]): Mode for parsing entities in the message text. See formatting options for more details.
+        entities (Optional[List[MessageEntity]]): A JSON-serialized list of special entities that appear in message text, which can be specified instead of `parse_mode`
+        disable_web_page_preview (Optional[bool]): Disables link previews for links in this message
+        disable_notification (Optional[bool]): Sends the message silently. Users will receive a notification with no sound.
+        protect_content (Optional[bool]): Protects the contents of the sent message from forwarding and saving
         reply_to_message_id (Optional[int]): [description]
         allow_sending_without_reply (Optional[bool]): [description]
         reply_markup (Optional[ Union[ InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove, ForceReply ] ]): [description]
@@ -119,9 +145,13 @@ def copyMessage(
         ]
     ],
 ) -> "MessageId":
-    """[summary]
+    """
 
-    [description]
+    Use this method to copy messages of any kind. Service messages and invoice messages can't be copied. 
+    A quiz poll can be copied only if the value of the field correct_option_id is known to the bot. 
+    The method is analogous to the method forwardMessage, 
+    but the copied message doesn't have a link to the original message.
+    Returns the MessageId of the sent message on success.
 
     Args:
         chat_id (Union[int, str]): [description]
@@ -158,9 +188,9 @@ def sendPhoto(
         ]
     ],
 ) -> "Message_":
-    """[summary]
+    """
 
-    [description]
+    Use this method to send photos. On success, the sent Message is returned.
 
     Args:
         chat_id (Union[int, str]): [description]
@@ -201,59 +231,127 @@ def sendAudio(
         ]
     ],
 ) -> "Message_":
-    """[summary]
+    """
 
-    [description]
+    Use this method to send audio files, if you want Telegram clients to display them in the music player. 
+    Your audio must be in the .MP3 or .M4A format. On success, the sent `Message` is returned. 
+    Bots can currently send audio files of up to 50 MB in size, this limit may be changed in the future.
+
+    For sending voice messages, use the `sendVoice` method instead.
 
     Args:
+        
         chat_id (Union[int, str]): [description]
+        
         message_thread_id (Optional[int]): [description]
+        
         audio (Union[InputFile, str]): [description]
+        
         caption (Optional[str]): [description]
+        
         parse_mod (Optional[str]): [description]
+        
         caption_entities (Optional[List[MessageEntity]]): [description]
+        
         duration (Optional[int]): [description]
+        
         performer (Optional[str]): [description]
+        
         title (Optional[str]): [description]
+        
         thumb (Optional[Union[InputFile, str]]): [description]
+        
         disable_notification (Optional[bool]): [description]
+        
         protect_content (Optional[bool]): [description]
+        
         reply_to_message_id (Optional[int]): [description]
+        
         allow_sending_without_reply (Optional[bool]): [description]
+        
         reply_markup (Optional[ Union[ InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove, ForceReply ] ]): [description]
     """
     pass
 
 
 def sendDocument():
+    """
+        Use this method to send general files. On success, the sent Message is returned. 
+        Bots can currently send files of any type of up to 50 MB in size, this limit may be changed in the future.
+    :see https://core.telegram.org/bots/api#sendDocument
+    """
     pass
 
 
 def sendVideo():
+    """
+        Use this method to send video files, Telegram clients support MPEG4 videos (other formats may be sent as Document). 
+        On success, the sent `Message` is returned. 
+        Bots can currently send video files of up to 50 MB in size, this limit may be changed in the future.
+    :see https://core.telegram.org/bots/api#sendVideo
+    """
     pass
 
 
 def sendVoice():
+    """
+        Use this method to send audio files, if you want Telegram clients to display the file as a playable voice message. For this to work, 
+        your audio must be in an .OGG file encoded with OPUS (other formats may be sent as Audio or Document). 
+        On success, the sent `Message` is returned. Bots can currently send voice messages of up to 50 MB in size, 
+        this limit may be changed in the future.
+    
+    :see https://core.telegram.org/bots/api#sendVoice
+    """
     pass
 
 
 def sendAnimation():
+    """
+        Use this method to send animation files (GIF or H.264/MPEG-4 AVC video without sound). 
+        On success, the sent Message is returned. 
+        Bots can currently send animation files of up to 50 MB in size, this limit may be changed in the future.
+    
+    :see https://core.telegram.org/bots/api#sendanimation
+
+    """
     pass
 
 
 def sendVideoNote():
+    """
+        As of v.4.0, Telegram clients support rounded square MPEG4 videos of up to 1 minute long. 
+        Use this method to send video messages. On success, the sent Message is returned.
+    :see https://core.telegram.org/bots/api#sendVideoNote
+    """
     pass
 
 
 def sendMediaGroup():
+    """
+        Use this method to send a group of photos, videos, documents or audios as an album. 
+        Documents and audio files can be only grouped in an album with messages of the same type. 
+        On success, an array of Messages that were sent is returned.
+    :see https://core.telegram.org/bots/api#sendMediaGroup
+    """
     pass
 
 
 def sendLocation():
+    """
+        Use this method to send point on the map. On success, the sent Message is returned.
+    :see https://core.telegram.org/bots/api#sendLocation
+    """
     pass
 
 
 def editMessageLiveLocation():
+    """
+        Use this method to edit live location messages. 
+        A location can be edited until its live_period expires or editing is explicitly disabled by a call to stopMessageLiveLocation. 
+        On success, if the edited message is not an inline message, the edited `Message` is returned, otherwise True is returned.
+    
+    :see https://core.telegram.org/bots/api#editMessageLiveLocation
+    """
     pass
 
 
@@ -277,6 +375,7 @@ def sendDice():
 
     pass
 
+List
 
 def sendChatAction() -> bool:
     pass
