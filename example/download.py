861513a67f8fa17c6a806866e5920f74d540bcc2
Jigsaw <j1g5aw@foxmail.com>
2023-01-09 08:06:01 +0000

    :memo: Add new example for download file

---
+++ example/download.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+from anyio import open_file
+from httpx import AsyncClient
+
+from nonebot import on_command
+from nonebot.adapters.telegram import Bot
+from nonebot.adapters.telegram.message import File
+from nonebot.adapters.telegram.event import MessageEvent
+
+
+@on_command("download").handle()
+async def _(bot: Bot, event: MessageEvent):
+    for seg in event.get_message():
+        if isinstance(seg, File):
+            file = await bot.get_file(file_id=seg.get("data")["file"])
+            url = f"https://api.telegram.org/file/bot{bot.bot_config.token}/{file.file_path}"
+            async with AsyncClient() as c:
+                async with await open_file(
+                    Path(file.file_path).name, "wb"  # type:ignore
+                ) as f:
+                    await f.write((await c.get(url)).content)
