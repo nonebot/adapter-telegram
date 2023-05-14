08dc020d2f0102f1e4b51db8c8e596cc598daadc
student_2333 <lgc2333@126.com>
2023-05-14 15:28:24 +0800

    翻新 @用户 示例

--- example/mention.py
+++ example/mention.py
@@ -4,46 +4,63 @@
 本示例所涉及的 Entity 在将在下个版本重构
 """
 
+from typing import Union
 from nonebot import on_command
 from nonebot.adapters.telegram import Bot
 from nonebot.adapters.telegram.message import Entity
-from nonebot.adapters.telegram.event import MessageEvent
+from nonebot.adapters.telegram.event import (
+    GroupMessageEvent,
+    PrivateMessageEvent,
+)
+from nonebot.adapters.telegram.model import User
+
+# ChannelPostEvent 没有 from 字段
+MessageEventHasUser = Union[PrivateMessageEvent, GroupMessageEvent]
+
+
+def format_nickname(user: User) -> str:
+    lastname = f" {user.last_name}" if user.last_name else ""
+    return f"{user.first_name}{lastname}"
+
 
 cmd_mention1 = on_command("mention1")
 
 
 @cmd_mention1.handle()
-async def _(bot: Bot, event: MessageEvent):
-    user = event.telegram_model.message.from_
+async def _(bot: Bot, event: MessageEventHasUser):
     # 获取一个 User 对象，这里从 event 获取用户的 User 对象
+    user = event.from_
 
-    if user is None:
-        return
-    # ChannelPostEvent 没有 from 字段
-
-    username = user.username
     # 获取用户名
+    username = user.username
 
-    await bot.send(event, Entity.mention("@" + username))
-    """要注意的是使用 Entity.mention 需要该用户有 username"""
+    if username:
+        # 要注意的是使用 Entity.mention 需要该用户有 username
+        await bot.send(event, Entity.mention(f"@{username}"))
 
-    await bot.send(
-        event, "first part " + Entity.mention("@" + username) + " second_part"
-    )
-    """
-    还要注意 Entity.mention 不能与其它消息直接相连，要用空格间隔开
-    @之前若与其他消息相连，则 Telegram 不会识别其为 mention
-    @之后若有其他消息相连，则 Telegram 会将后面的消息也识别为 username 的一部分
-    """
+        # 还要注意 Entity.mention 不能与其它消息直接相连，要用空格间隔开
+        # @ 之前若与其他消息相连，则 Telegram 不会识别其为 mention
+        # @ 之后若有其他消息相连，则 Telegram 会将后面的消息也识别为 username 的一部分
+        await bot.send(
+            event, "first part " + Entity.mention(f"@{username}") + " second_part"
+        )
+
+    else:
+        # 当用户没有用户名时，请使用 text_link 或 text_mention 进行 @
+        nick = format_nickname(user)
+        await bot.send(
+            event,
+            "Hello, " + Entity.text_link(nick, f"tg://user?id={user.id}") + "!",
+        )
+        await bot.send(event, Entity.text_mention(nick, user) + ", How are you?")
 
 
 cmd_mention2 = on_command("mention2")
 
 
 @cmd_mention2.handle()
-async def _(bot: Bot, event: MessageEvent):
-    if (user := event.telegram_model.message.from_) is None:
-        return
+async def _(bot: Bot, event: MessageEventHasUser):
+    user = event.from_
 
-    await bot.send(event, Entity.text_mention("anytext", user))
     # 与 mention 不同的是，text_mention 的文本可以自定义
+    await bot.send(event, Entity.text_mention("anytext", user))
